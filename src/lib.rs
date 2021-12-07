0706d6e22dc2c3d765258c49b45a42c7046272df
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2021-12-07 15:56:20 -0500

    Begin implementing traits and error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
+//! `Rgba_simple` is a small library for storing colors in RGBA and Hex notation.
+//! It includes functions to convert to and from Hex and RGBA. All of the internal
+//! formats can be serialized and deserialized with `serde`. If compiled with the
+//! `gtk` feature, all of it's internal representations can also be converted to
+//! and from `gtk::gdk::RGBA`, making one use case storing colors generated from
+//! a Gtk+ gui in a config file, using one of the many formats with `serde`
+//! support.
 #[cfg(feature = "gtk")]
 use gtk::gdk;
 use serde::{Deserialize, Serialize};
+
 /// This struct contains a color represented in hex notation plus an opacity
 /// value. This is necessary to represent colors in an SVG image
 #[derive(Clone, Deserialize, Debug, Serialize)]
@@ -38,99 +46,74 @@ pub enum Color {
     Rgba(RGBA),
 }
 
-impl Color {
-    pub fn to_hex(&self) -> HexColor {
+#[derive(Clone, Debug, PartialEq)]
+pub enum ColorBoundsError {
+    Negative,
+    High,
+}
+
+pub trait ToHex {
+    type Err;
+    fn to_hex(&self) -> Result<HexColor, Self::Err>;
+}
+
+impl ToHex for ReducedRGBA {
+    type Err = ColorBoundsError;
+
+    fn to_hex(&self) -> Result<HexColor, Self::Err> {
+        Ok(HexColor {
+            color: format!("#{:02x}{:02x}{:02x}", self.red, self.green, self.blue,),
+            alpha: self.alpha as f32 / 255.0,
+        })
+    }
+}
+
+impl ToHex for RGBA {
+    type Err = ColorBoundsError;
+
+    /// Converts an [RGBA] color (red, green, blue plus alpha) to a struct
+    /// containing a hex color string and an opacity value, suitable for
+    /// embedding into an svg image
+    fn to_hex(&self) -> Result<HexColor, Self::Err> {
+        if self.red < 0.0 || self.green < 0.0 || self.blue < 0.0 {
+            Err(ColorBoundsError::Negative)
+        } else if self.red > 1.0 || self.green > 1.0 || self.green > 1.0 {
+            Err(ColorBoundsError::High)
+        } else {
+            Ok(HexColor {
+                color: format!(
+                    "#{:02x}{:02x}{:02x}",
+                    (self.red * 255.0) as u8,
+                    (self.green * 255.0) as u8,
+                    (self.blue * 255.0) as u8,
+                ),
+                alpha: self.alpha,
+            })
+        }
+    }
+}
+
+impl ToHex for Color {
+    type Err = ColorBoundsError;
+
+    fn to_hex(&self) -> Result<HexColor, ColorBoundsError> {
         match self {
-            Color::Hex(c) => HexColor {
+            Color::Hex(c) => Ok(HexColor {
                 color: c.color.clone(),
                 alpha: c.alpha,
-            },
+            }),
             Color::Rgba(c) => c.to_hex(),
             Color::Reduced(c) => c.to_hex(),
         }
     }
-
-    #[cfg(feature = "gtk")]
-    pub fn to_gdk(&self) -> Option<gdk::RGBA> {
-        match self {
-            Color::Hex(c) => c.to_gdk(),
-            Color::Rgba(c) => Some(c.to_gdk()),
-            Color::Reduced(c) => Some(c.to_gdk()),
-        }
-    }
 }
 
-impl HexColor {
-    pub fn to_reduced(&self) -> Option<ReducedRGBA> {
-        if let Ok(buf) = hex::decode(&self.color[1..]) {
-            return Some(ReducedRGBA {
-                red: buf[0],
-                green: buf[1],
-                blue: buf[2],
-                alpha: (self.alpha * 255.0) as u8,
-            });
-        }
-        None
-    }
-
-    pub fn to_rgba(&self) -> Option<RGBA> {
-        self.to_reduced().map(|color| color.to_rgba())
-    }
-
-    #[cfg(feature = "gtk")]
-    pub fn to_gdk(&self) -> Option<gdk::RGBA> {
-        if let Some(color) = self.to_reduced() {
-            Some(color.to_gdk())
-        } else {
-            None
-        }
-    }
-
-    pub fn black() -> HexColor {
-        HexColor {
-            color: String::from("#000000"),
-            alpha: 1.0,
-        }
-    }
-
-    pub fn white() -> HexColor {
-        HexColor {
-            color: String::from("#ffffff"),
-            alpha: 1.0,
-        }
-    }
-
-    pub fn red() -> HexColor {
-        HexColor {
-            color: String::from("#ff0000"),
-            alpha: 1.0,
-        }
-    }
-
-    pub fn green() -> HexColor {
-        HexColor {
-            color: String::from("#00ff00"),
-            alpha: 1.0,
-        }
-    }
-
-    pub fn blue() -> HexColor {
-        HexColor {
-            color: String::from("#0000ff"),
-            alpha: 1.0,
-        }
-    }
+pub trait ToRGBA {
+    fn to_rgba(&self) -> RGBA;
 }
 
-impl ReducedRGBA {
-    pub fn to_hex(&self) -> HexColor {
-        HexColor {
-            color: format!("#{:02x}{:02x}{:02x}", self.red, self.green, self.blue,),
-            alpha: self.alpha as f32 / 255.0,
-        }
-    }
-
-    pub fn to_rgba(&self) -> RGBA {
+impl ToRGBA for ReducedRGBA {
+    fn to_rgba(&self) -> RGBA {
         RGBA {
             red: f32::from(self.red) / 255.0,
             green: f32::from(self.green) / 255.0,
@@ -138,8 +121,24 @@ impl ReducedRGBA {
             alpha: f32::from(self.alpha) / 255.0,
         }
     }
+}
 
-    #[cfg(feature = "gtk")]
+pub trait ToReducedRGBA {
+    fn to_reduced_rgba(&self);
+}
+
+#[cfg(feature = "gtk")]
+pub trait ToGdk {
+    fn to_gdk(&self) -> gdk::RGBA;
+}
+
+#[cfg(feature = "gtk")]
+pub trait FromGdk {
+    fn from_gdk(color: &gdk::RGBA) -> Self;
+}
+
+#[cfg(feature = "gtk")]
+impl ToGdk for ReducedRGBA {
     pub fn to_gdk(&self) -> gdk::RGBA {
         gdk::RGBA {
             red: f32::from(self.red) / 255.0,
@@ -148,7 +147,9 @@ impl ReducedRGBA {
             alpha: f32::from(self.alpha) / 255.0,
         }
     }
+}
 
+impl ReducedRGBA {
     #[cfg(feature = "gtk")]
     pub fn from_gdk(color: &gdk::RGBA) -> ReducedRGBA {
         RGBA {
@@ -207,21 +208,6 @@ impl ReducedRGBA {
 }
 
 impl RGBA {
-    /// Converts an [RGBA] color (red, green, blue plus alpha) to a struct
-    /// containing a hex color string and an opacity value, suitable for
-    /// embedding into an svg image
-    pub fn to_hex(&self) -> HexColor {
-        HexColor {
-            color: format!(
-                "#{:02x}{:02x}{:02x}",
-                (self.red * 255.0) as u8,
-                (self.green * 255.0) as u8,
-                (self.blue * 255.0) as u8,
-            ),
-            alpha: self.alpha,
-        }
-    }
-
     pub fn to_reduced(&self) -> ReducedRGBA {
         ReducedRGBA {
             red: (self.red * 255.0) as u8,
@@ -332,38 +318,111 @@ impl RGBA {
     }
 }
 
+impl Color {
+    #[cfg(feature = "gtk")]
+    pub fn to_gdk(&self) -> Option<gdk::RGBA> {
+        match self {
+            Color::Hex(c) => c.to_gdk(),
+            Color::Rgba(c) => Some(c.to_gdk()),
+            Color::Reduced(c) => Some(c.to_gdk()),
+        }
+    }
+}
+
+impl HexColor {
+    pub fn to_reduced(&self) -> Option<ReducedRGBA> {
+        if let Ok(buf) = hex::decode(&self.color[1..]) {
+            return Some(ReducedRGBA {
+                red: buf[0],
+                green: buf[1],
+                blue: buf[2],
+                alpha: (self.alpha * 255.0) as u8,
+            });
+        }
+        None
+    }
+
+    pub fn to_rgba(&self) -> Option<RGBA> {
+        self.to_reduced().map(|color| color.to_rgba())
+    }
+
+    #[cfg(feature = "gtk")]
+    pub fn to_gdk(&self) -> Option<gdk::RGBA> {
+        if let Some(color) = self.to_reduced() {
+            Some(color.to_gdk())
+        } else {
+            None
+        }
+    }
+
+    pub fn black() -> HexColor {
+        HexColor {
+            color: String::from("#000000"),
+            alpha: 1.0,
+        }
+    }
+
+    pub fn white() -> HexColor {
+        HexColor {
+            color: String::from("#ffffff"),
+            alpha: 1.0,
+        }
+    }
+
+    pub fn red() -> HexColor {
+        HexColor {
+            color: String::from("#ff0000"),
+            alpha: 1.0,
+        }
+    }
+
+    pub fn green() -> HexColor {
+        HexColor {
+            color: String::from("#00ff00"),
+            alpha: 1.0,
+        }
+    }
+
+    pub fn blue() -> HexColor {
+        HexColor {
+            color: String::from("#0000ff"),
+            alpha: 1.0,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn black() {
-        assert_eq!(RGBA::black().to_hex().color, HexColor::black().color);
-        assert_eq!(ReducedRGBA::black().to_hex().color, HexColor::black().color);
+        assert_eq!(RGBA::black().to_hex().unwrap().color, HexColor::black().color);
+        assert_eq!(ReducedRGBA::black().to_hex().unwrap().color, HexColor::black().color);
     }
 
     #[test]
     fn white() {
-        assert_eq!(RGBA::white().to_hex().color, HexColor::white().color);
-        assert_eq!(ReducedRGBA::white().to_hex().color, HexColor::white().color);
+        assert_eq!(RGBA::white().to_hex().unwrap().color, HexColor::white().color);
+        assert_eq!(ReducedRGBA::white().to_hex().unwrap().color, HexColor::white().color);
     }
 
     #[test]
     fn red() {
-        assert_eq!(RGBA::red().to_hex().color, HexColor::red().color);
-        assert_eq!(ReducedRGBA::red().to_hex().color, HexColor::red().color);
+        assert_eq!(RGBA::red().to_hex().unwrap().color, HexColor::red().color);
+        assert_eq!(ReducedRGBA::red().to_hex().unwrap().color, HexColor::red().color);
     }
 
     #[test]
     fn green() {
-        assert_eq!(RGBA::green().to_hex().color, HexColor::green().color);
-        assert_eq!(ReducedRGBA::green().to_hex().color, HexColor::green().color);
+        assert_eq!(RGBA::green().to_hex().unwrap().color, HexColor::green().color);
+        assert_eq!(ReducedRGBA::green().to_hex().unwrap().color, HexColor::green().color);
     }
 
     #[test]
     fn blue() {
-        assert_eq!(RGBA::blue().to_hex().color, HexColor::blue().color);
-        assert_eq!(ReducedRGBA::blue().to_hex().color, HexColor::blue().color);
+        assert_eq!(RGBA::blue().to_hex().unwrap().color, HexColor::blue().color);
+        assert_eq!(ReducedRGBA::blue().to_hex().unwrap().color, HexColor::blue().color);
     }
 
     #[test]
